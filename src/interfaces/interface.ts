140d58d7f2de1908051e175c8154e0c3e382a05c
jinho1011 <gino9940@gmail.com>
2022-01-30 20:46:14 +0900

    Add interfaces

---
+++ src/interfaces/interface.ts
@@ -0,0 +1,35 @@
+export interface State {
+  isReady: boolean;
+  currentWeather: any;
+  forecast: any;
+  backgroundColor: string;
+}
+
+export interface Coords {
+  lat: number;
+  lng: number;
+}
+
+export interface Weather {
+  description: string;
+  icon: string;
+  id: number;
+  main: string;
+}
+
+export interface Hourly {
+  clouds: number;
+  dew_point: number;
+  dt: number;
+  feels_like: number;
+  humidity: number;
+  pop: number;
+  pressure: number;
+  temp: number;
+  uvi: number;
+  visibility: number;
+  weather: Weather[];
+  wind_deg: number;
+  wind_gust: number;
+  wind_speed: number;
+}
