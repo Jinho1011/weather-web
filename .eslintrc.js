d666babe3e422745194c4b1ee7de845d000ac220
jinho1011 <gino9940@gmail.com>
2022-01-30 20:43:39 +0900

    Add libraries

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
-  ],
+  extends: "plugin:react/recommended",
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
