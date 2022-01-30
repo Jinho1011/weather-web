2d24344f10fe644a3f0d381d339bd3ab12da4a44
jinho1011 <gino9940@gmail.com>
2022-01-30 20:46:02 +0900

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,67 @@
 import React, { useState, useEffect } from "react";
+import { css, cx } from "@emotion/css";
+import { getCoords, getWeather, getOneCall, getCurrentColor } from "./api";
+import { State } from "./interfaces/interface";
+
+import Header from "./modules/header";
+import Today from "./modules/today";
 
 const App: React.FC = () => {
-  const [state, setState] = useState(0);
+  const [state, setState] = useState<State>({
+    isReady: false,
+    currentWeather: {},
+    forecast: {},
+    backgroundColor: getCurrentColor(),
+  });
 
-  return <div className="App"></div>;
+  useEffect(() => {
+    const init = async () => {
+      const coord = await getCoords();
+      const currentWeather = await getWeather(coord.lat, coord.lng);
+      const forecast = await getOneCall(coord.lat, coord.lng);
+      setState((prev: any) => ({
+        ...prev,
+        currentWeather,
+        forecast,
+        isReady: true,
+      }));
+    };
+    const interval = setInterval(() => {
+      setState((prev: any) => ({
+        ...prev,
+        backgroundColor: getCurrentColor(),
+      }));
+    }, 60000);
+    init();
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    console.log("🚀 ~ file: App.tsx ~ line 21 ~ state", state);
+  }, [state]);
+
+  return (
+    <>
+      {state.isReady ? (
+        <div
+          className={css`
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            background-color: ${state.backgroundColor};
+          `}
+        >
+          <Header state={state} />
+          <Today state={state} />
+        </div>
+      ) : (
+        "Ready"
+      )}
+    </>
+  );
 };
 
 export default App;
